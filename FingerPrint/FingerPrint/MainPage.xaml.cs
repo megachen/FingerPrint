a389caaa7bd0cf33e275962efa30140fb4a86145
megachen <maxonmegachen@outlook.com>
2015-03-31 00:28:34 +0800

    add simple functions

--- FingerPrint/FingerPrint/MainPage.xaml.cs
+++ FingerPrint/FingerPrint/MainPage.xaml.cs
@@ -18,113 +18,27 @@ namespace FingerPrint
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        private Point old_point, old_vect;
-        private int dispindex;
-        private double stat, last_ang;
-        private Ellipse last_el;
-        private Line last_line;
-        private bool draw = false;
+        private DrawHelper draw;
 
         // 构造函数
         public MainPage()
         {
             InitializeComponent();
 
-            old_point = new Point(0, 0);
             // 用于本地化 ApplicationBar 的示例代码
             BuildLocalizedApplicationBar();
-        }
 
-        private void OnPressed(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            old_point = e.GetPosition(paint);
-            old_vect.X = 0; old_vect.Y = 0;
-            dispindex = 0;
-            stat = 0.0;
-            disp.Children.Clear();
-            paint.Children.Clear();
-            Line line = new Line();
-            line.X1 = 0;
-            line.Y1 = disp.ActualHeight/2;
-            line.X2 = disp.ActualWidth;
-            line.Y2 = disp.ActualHeight / 2;
-            line.Stroke = new SolidColorBrush(Color.FromArgb(255, 200, 200, 0));
-            line.StrokeThickness = 1;
-            line.Opacity = 1;
-            disp.Children.Add(line);
-            draw = true;
-        }
-
-        private void OnMoving(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            Point point = e.GetPosition(paint);
-            if (Dist(point, old_point) < 8) return;
-            if (!draw) { old_point = point; return; }
-            {
-                //draw line
-                Line line = new Line();
-
-                line.X1 = point.X;
-                line.Y1 = point.Y;
-                line.X2 = old_point.X;
-                line.Y2 = old_point.Y;
-
-                line.Stroke = new SolidColorBrush(Color.FromArgb(255, 41, 159, 227));
-                line.StrokeStartLineCap = PenLineCap.Round;
-                line.StrokeEndLineCap = PenLineCap.Round;
-
-                line.StrokeThickness = 15;
-                line.Opacity = 1;
-
-                paint.Children.Add(line);
-
-                //analyze
-                Point vect = new Point(point.X - old_point.X, point.Y - old_point.Y);
-                if (vect.X == 0.0 && vect.Y == 0.0) return;
-
-                if (!(old_vect.X == 0.0 && old_vect.Y == 0.0))
-                {
-                    double ang = Angles(vect, old_vect);
-                    if (double.IsNaN(ang)) return;
-                    stat += ang;
-                    msg.Text = ang.ToString() + "           " + stat.ToString();
-                    Ellipse pt = new Ellipse();
-                    pt.Fill = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
-                    pt.Width = pt.Height = 5;
-                    disp.Children.Add(pt);
-                    Canvas.SetLeft(pt, dispindex++ * 6.0);
-                    if (ang > 85.0) ang = 85.0;
-                    if (ang < -85.0) ang = -85.0;
-                    Canvas.SetTop(pt, 90.0 - ang);
-                    last_ang = ang;
-                    last_el = pt;
-                    last_line = line;
-                }
-                
-                //finalize
-                old_point = point;
-                old_vect = vect;
-            }
-            
-        }
-
-        private void OnReleased(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            disp.Children.Remove(last_el);
-            stat -= last_ang;
-            paint.Children.Remove(last_line);
-            msg.Text = last_ang.ToString() + "           " + stat.ToString();
-            draw = false;
+            draw = new DrawHelper(paint);
         }
 
         private void OnPageLoaded(object sender, RoutedEventArgs e)
         {
-            paint.MouseMove += new MouseEventHandler(OnMoving);
-            paint.MouseLeftButtonUp += new MouseButtonEventHandler(OnReleased);
-            paint.MouseLeftButtonDown += new MouseButtonEventHandler(OnPressed);
+            //paint.MouseMove += new MouseEventHandler(OnMoving);
+            //paint.MouseLeftButtonUp += new MouseButtonEventHandler(OnReleased);
+            //paint.MouseLeftButtonDown += new MouseButtonEventHandler(OnPressed);
         }
 
-         //用于生成本地化 ApplicationBar 的示例代码
+        //用于生成本地化 ApplicationBar 的示例代码
         private void BuildLocalizedApplicationBar()
         {
             // 将页面的 ApplicationBar 设置为 ApplicationBar 的新实例。
@@ -154,29 +68,127 @@ namespace FingerPrint
             ApplicationBar.MenuItems.Add(appMenu_clear);
         }
 
+        private void OnPressed(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            draw.Pressdown(e.GetPosition(paint));
+            (sender as Canvas).CaptureMouse();
+
+            //old_vect.X = 0; old_vect.Y = 0;
+            //dispindex = 0;
+            //stat = 0.0;
+            //disp.Children.Clear();
+            //paint.Children.Clear();
+            //Line line = new Line();
+            //line.X1 = 0;
+            //line.Y1 = disp.ActualHeight/2;
+            //line.X2 = disp.ActualWidth;
+            //line.Y2 = disp.ActualHeight / 2;
+            //line.Stroke = new SolidColorBrush(Color.FromArgb(255, 200, 200, 0));
+            //line.StrokeThickness = 1;
+            //line.Opacity = 1;
+            //disp.Children.Add(line);
+            //draw = true;
+        }
+
+        private void OnMoving(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            draw.Move(e.GetPosition(paint));
+            //Point point = e.GetPosition(paint);
+            //if (Dist(point, old_point) < 8) return;
+            //if (!draw) { old_point = point; return; }
+            //{
+            //    //draw line
+            //    Line line = new Line();
+
+            //    line.X1 = point.X;
+            //    line.Y1 = point.Y;
+            //    line.X2 = old_point.X;
+            //    line.Y2 = old_point.Y;
+
+            //    line.Stroke = new SolidColorBrush(Color.FromArgb(255, 41, 159, 227));
+            //    line.StrokeStartLineCap = PenLineCap.Round;
+            //    line.StrokeEndLineCap = PenLineCap.Round;
+
+            //    line.StrokeThickness = 15;
+            //    line.Opacity = 1;
+
+            //    paint.Children.Add(line);
+
+            //    //analyze
+            //    Point vect = new Point(point.X - old_point.X, point.Y - old_point.Y);
+            //    if (vect.X == 0.0 && vect.Y == 0.0) return;
+
+            //    if (!(old_vect.X == 0.0 && old_vect.Y == 0.0))
+            //    {
+            //        double ang = Angles(vect, old_vect);
+            //        if (double.IsNaN(ang)) return;
+            //        stat += ang;
+            //        msg.Text = ang.ToString() + "           " + stat.ToString();
+            //        Ellipse pt = new Ellipse();
+            //        pt.Fill = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+            //        pt.Width = pt.Height = 5;
+            //        disp.Children.Add(pt);
+            //        Canvas.SetLeft(pt, dispindex++ * 6.0);
+            //        if (ang > 85.0) ang = 85.0;
+            //        if (ang < -85.0) ang = -85.0;
+            //        Canvas.SetTop(pt, 90.0 - ang);
+            //        last_ang = ang;
+            //        last_el = pt;
+            //        last_line = line;
+            //    }
+                
+            //    //finalize
+            //    old_point = point;
+            //    old_vect = vect;
+            //}
+            
+        }
+
+        private void OnReleased(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            draw.Release(e.GetPosition(paint));
+            (sender as Canvas).ReleaseMouseCapture();
+            //disp.Children.Remove(last_el);
+            //stat -= last_ang;
+            //paint.Children.Remove(last_line);
+            //msg.Text = last_ang.ToString() + "           " + stat.ToString();
+            //draw = false;
+        }
+
         void appMenu_clear_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            draw.Clearboard();
+            Showtips(AppResources.FO_clear);
         }
 
         void appBtn_smart_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            draw.Changemode(Drawstate.Smart);
+            Showtips(AppResources.DT_smart);
         }
 
         void appBtn_free_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            draw.Changemode(Drawstate.Free);
+            Showtips(AppResources.DT_free);
         }
 
         void appBtn_circle_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            draw.Changemode(Drawstate.Circle);
+            Showtips(AppResources.DT_circle);
         }
 
         void appBtn_line_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            draw.Changemode(Drawstate.Line);
+            Showtips(AppResources.DT_line);
+        }
+
+        void Showtips(string content)
+        {
+            tips.Text = content;
+            tipAnime.Begin();
         }
 
         private double Dist(Point a, Point b)
