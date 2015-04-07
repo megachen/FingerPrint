58216eff5d34a85903da2e6bfd6080a8baf12385
megachen <maxonmegachen@outlook.com>
2015-04-08 00:16:11 +0800

    mod home menu

--- FingerPrint/FingerPrint/MainPage.xaml.cs
+++ FingerPrint/FingerPrint/MainPage.xaml.cs
@@ -19,6 +19,7 @@ namespace FingerPrint
     public partial class MainPage : PhoneApplicationPage
     {
         private DrawHelper draw;
+        private bool menuState;
 
         // 构造函数
         public MainPage()
@@ -26,58 +27,59 @@ namespace FingerPrint
             InitializeComponent();
 
             // 用于本地化 ApplicationBar 的示例代码
-            BuildLocalizedApplicationBar();
+            //BuildLocalizedApplicationBar();
 
-            draw = new DrawHelper(paint);
+            draw = new DrawHelper(cnv_paint);
+            menuState = false;
         }
 
         private void OnPageLoaded(object sender, RoutedEventArgs e)
         {
-            //paint.MouseMove += new MouseEventHandler(OnMoving);
-            //paint.MouseLeftButtonUp += new MouseButtonEventHandler(OnReleased);
-            //paint.MouseLeftButtonDown += new MouseButtonEventHandler(OnPressed);
+            //cnv_paint.MouseMove += new MouseEventHandler(OnMoving);
+            //cnv_paint.MouseLeftButtonUp += new MouseButtonEventHandler(OnReleased);
+            //cnv_paint.MouseLeftButtonDown += new MouseButtonEventHandler(OnPressed);
         }
 
         //用于生成本地化 ApplicationBar 的示例代码
-        private void BuildLocalizedApplicationBar()
-        {
-            // 将页面的 ApplicationBar 设置为 ApplicationBar 的新实例。
-            ApplicationBar = new ApplicationBar();
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // 将页面的 ApplicationBar 设置为 ApplicationBar 的新实例。
+        //    ApplicationBar = new ApplicationBar();
 
-            // 创建新按钮并将文本值设置为 AppResources 中的本地化字符串。
-            ApplicationBarIconButton appBtn_line = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
-            appBtn_line.Text = AppResources.DT_line;
-            appBtn_line.Click += appBtn_line_Click;
-            ApplicationBarIconButton appBtn_circle = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
-            appBtn_circle.Text = AppResources.DT_circle;
-            appBtn_circle.Click += appBtn_circle_Click;
-            ApplicationBarIconButton appBtn_free = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
-            appBtn_free.Text = AppResources.DT_free;
-            appBtn_free.Click += appBtn_free_Click;
-            ApplicationBarIconButton appBtn_smart = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
-            appBtn_smart.Text = AppResources.DT_smart;
-            appBtn_smart.Click += appBtn_smart_Click;
-            ApplicationBar.Buttons.Add(appBtn_line);
-            ApplicationBar.Buttons.Add(appBtn_circle);
-            ApplicationBar.Buttons.Add(appBtn_free);
-            ApplicationBar.Buttons.Add(appBtn_smart);
+        //    // 创建新按钮并将文本值设置为 AppResources 中的本地化字符串。
+        //    ApplicationBarIconButton appBtn_line = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
+        //    appBtn_line.Text = AppResources.DT_line;
+        //    appBtn_line.Click += appBtn_line_Click;
+        //    ApplicationBarIconButton appBtn_circle = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
+        //    appBtn_circle.Text = AppResources.DT_circle;
+        //    appBtn_circle.Click += appBtn_circle_Click;
+        //    ApplicationBarIconButton appBtn_free = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
+        //    appBtn_free.Text = AppResources.DT_free;
+        //    appBtn_free.Click += appBtn_free_Click;
+        //    ApplicationBarIconButton appBtn_smart = new ApplicationBarIconButton(new Uri("/Assets/Icons/edit.png", UriKind.Relative));
+        //    appBtn_smart.Text = AppResources.DT_smart;
+        //    appBtn_smart.Click += appBtn_smart_Click;
+        //    ApplicationBar.Buttons.Add(appBtn_line);
+        //    ApplicationBar.Buttons.Add(appBtn_circle);
+        //    ApplicationBar.Buttons.Add(appBtn_free);
+        //    ApplicationBar.Buttons.Add(appBtn_smart);
 
-            // 使用 AppResources 中的本地化字符串创建新菜单项。
-            ApplicationBarMenuItem appMenu_clear = new ApplicationBarMenuItem(AppResources.FO_clear);
-            appMenu_clear.Click += appMenu_clear_Click;
-            ApplicationBar.MenuItems.Add(appMenu_clear);
-        }
+        //    // 使用 AppResources 中的本地化字符串创建新菜单项。
+        //    ApplicationBarMenuItem appMenu_clear = new ApplicationBarMenuItem(AppResources.FO_clear);
+        //    appMenu_clear.Click += appMenu_clear_Click;
+        //    ApplicationBar.MenuItems.Add(appMenu_clear);
+        //}
 
         private void OnPressed(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            draw.Pressdown(e.GetPosition(paint));
+            draw.Pressdown(e.GetPosition(cnv_paint));
             (sender as Canvas).CaptureMouse();
 
             //old_vect.X = 0; old_vect.Y = 0;
             //dispindex = 0;
             //stat = 0.0;
             //disp.Children.Clear();
-            //paint.Children.Clear();
+            //cnv_paint.Children.Clear();
             //Line line = new Line();
             //line.X1 = 0;
             //line.Y1 = disp.ActualHeight/2;
@@ -92,8 +94,8 @@ namespace FingerPrint
 
         private void OnMoving(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            draw.Move(e.GetPosition(paint));
-            //Point point = e.GetPosition(paint);
+            draw.Move(e.GetPosition(cnv_paint));
+            //Point point = e.GetPosition(cnv_paint);
             //if (Dist(point, old_point) < 8) return;
             //if (!draw) { old_point = point; return; }
             //{
@@ -112,7 +114,7 @@ namespace FingerPrint
             //    line.StrokeThickness = 15;
             //    line.Opacity = 1;
 
-            //    paint.Children.Add(line);
+            //    cnv_paint.Children.Add(line);
 
             //    //analyze
             //    Point vect = new Point(point.X - old_point.X, point.Y - old_point.Y);
@@ -146,51 +148,74 @@ namespace FingerPrint
 
         private void OnReleased(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            draw.Release(e.GetPosition(paint));
+            draw.Release(e.GetPosition(cnv_paint));
             (sender as Canvas).ReleaseMouseCapture();
             //disp.Children.Remove(last_el);
             //stat -= last_ang;
-            //paint.Children.Remove(last_line);
+            //cnv_paint.Children.Remove(last_line);
             //msg.Text = last_ang.ToString() + "           " + stat.ToString();
             //draw = false;
         }
 
-        void appMenu_clear_Click(object sender, EventArgs e)
+        void appMenu_clear_Click(object sender, RoutedEventArgs e)
         {
+            MenuClose();
             draw.Clearboard();
             Showtips(AppResources.FO_clear);
         }
 
-        void appBtn_smart_Click(object sender, EventArgs e)
+        void appBtn_smart_Click(object sender, RoutedEventArgs e)
         {
+            MenuClose();
             draw.Changemode(Drawstate.Smart);
             Showtips(AppResources.DT_smart);
         }
 
-        void appBtn_free_Click(object sender, EventArgs e)
+        void appBtn_free_Click(object sender, RoutedEventArgs e)
         {
+            MenuClose();
             draw.Changemode(Drawstate.Free);
             Showtips(AppResources.DT_free);
         }
 
-        void appBtn_circle_Click(object sender, EventArgs e)
+        void appBtn_circle_Click(object sender, RoutedEventArgs e)
         {
+            MenuClose();
             draw.Changemode(Drawstate.Circle);
             Showtips(AppResources.DT_circle);
         }
 
-        void appBtn_line_Click(object sender, EventArgs e)
+        void appBtn_line_Click(object sender, RoutedEventArgs e)
         {
+            MenuClose();
             draw.Changemode(Drawstate.Line);
             Showtips(AppResources.DT_line);
         }
 
         void Showtips(string content)
         {
-            tips.Text = content;
+            txt_tips.Text = content;
             tipAnime.Begin();
         }
 
+        private void OnClkMenu(object sender, RoutedEventArgs e)
+        {
+            if (!menuState) MenuOpen();
+            else MenuClose();
+        }
+
+        private void MenuOpen()
+        {
+            menuState = true;
+            Showtips("Menu");
+            menuOpenAnime.Begin();
+        }
+
+        private void MenuClose()
+        {
+            menuState = false;
+            menuCloseAnime.Begin();
+        }
         //private double Dist(Point a, Point b)
         //{
         //    return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
